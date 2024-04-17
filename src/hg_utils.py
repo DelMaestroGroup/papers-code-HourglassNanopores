9b82196830b679653624ec687197676a4e90df30
Adrian Del Maestro <adrian@delmaestro.org>
2024-04-17 11:11:42 -0400

    latest updated data

---
+++ src/hg_utils.py
@@ -0,0 +1,19 @@
+# hourglass nanopores analysis helper utilities
+
+import os
+
+pimc_bin_path = os.environ['HOME'] + '/.conda/envs/pimc/bin'
+data_dir = '/lustre/isaac/scratch/agdelma/Projects/HourGlass/w_eq_3.0'
+gp = os.environ['HOME'] + '/local/bin/parallel'
+
+def lab(_ΔR):
+    return f'dR_eq_{_ΔR:3.1f}'
+
+def base_dir(_ΔR,cylinder=True):
+    base = f'{data_dir}/{lab(_ΔR)}/OUTPUT/MERGED'
+    if cylinder:
+        base += '/CYLINDER'
+    return base
+
+def out_name(_ΔR):
+    return f'dR_eq_{_ΔR:3.1f}'
