9c2cf2a60d75f81c0fd88c92c651b0826337bd07
Adrian Del Maestro <adrian@delmaestro.org>
2024-08-19 10:28:14 -0400

    modified fitting and error analysis

--- src/hg_utils.py
+++ src/hg_utils.py
@@ -1,6 +1,7 @@
 # hourglass nanopores analysis helper utilities
 
 import os
+import numpy as np
 
 pimc_bin_path = os.environ['HOME'] + '/.conda/envs/pimc/bin'
 data_dir = '/lustre/isaac/scratch/agdelma/Projects/HourGlass/w_eq_3.0'
@@ -20,3 +21,17 @@ def repo_dir(_ΔR,cylinder=True):
     if cylinder:
         base += '/CYLINDER'
     return base
+
+def shifted_ave(y,Δy,ȳ,Δȳ):
+    """The error in a shifted mean from https://en.wikipedia.org/wiki/Propagation_of_uncertainty"""
+
+    f = y/ȳ - 1.0
+    
+    # First we compute the error in the numerator y-ȳ
+    num = y-ȳ
+    Δnum = np.sqrt(Δy**2 + Δȳ**2)
+
+    # now we computer the error in (y-ȳ)/ȳ
+    Δf = np.abs(f)*np.sqrt((Δnum/num)**2 + (Δȳ/ȳ)**2) 
+    
+    return f, Δf
