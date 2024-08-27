bc86de30045ff911059be1e7eb264f56fd7e5966
Adrian Del Maestro <adrian@delmaestro.org>
2024-08-27 16:29:31 -0400

    added new reduced file

--- src/hg_utils.py
+++ src/hg_utils.py
@@ -2,10 +2,11 @@
 
 import os
 import numpy as np
+import json
 
-pimc_bin_path = os.environ['HOME'] + '/.conda/envs/pimc/bin'
-data_dir = '/lustre/isaac/scratch/agdelma/Projects/HourGlass/w_eq_3.0'
-gp = os.environ['HOME'] + '/local/bin/parallel'
+#pimc_bin_path = os.environ['HOME'] + '/.conda/envs/pimc/bin'
+#data_dir = '/lustre/isaac/scratch/agdelma/Projects/HourGlass/w_eq_3.0'
+#gp = os.environ['HOME'] + '/local/bin/parallel'
 
 def lab(_ΔR):
     return f'dR_eq_{_ΔR:3.1f}'
@@ -35,3 +36,22 @@ def shifted_ave(y,Δy,ȳ,Δȳ):
     Δf = np.abs(f)*np.sqrt((Δnum/num)**2 + (Δȳ/ȳ)**2) 
     
     return f, Δf
+
+# we load relavent local path information from a file if it exists, if not, we query the user
+
+try:
+    with open('../include/local.json', 'r') as f:
+        local = json.load(f)
+        pimc_bin_path = local['pimc_bin_path']
+        data_dir = local['data_dir']
+        gnu_parallel = local['gnu_parallel']
+
+except:
+    pimc_bin_path = input("Enter path to `pimcscripts` executables (see e.g. https://github.com/DelMaestroGroup/pimcscripts): ")
+    data_dir = input("Enter path to raw QMC Data (e.g. '../data/qmc/'): ")
+    gnu_parallel = input("Enter path to gnu parallel (e.g. '/local/bin/parallel'): ")
+
+    local = {'pimc_bin_path':pimc_bin_path, 'data_dir':data_dir, 'gnu_parallel':gnu_parallel}
+    
+    with open('../include/local.json', 'w') as f:
+        json.dump(local, f, ensure_ascii=True, indent=4)
